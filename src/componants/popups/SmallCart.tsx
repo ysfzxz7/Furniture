bbade83b9a5df82b413ef00e144a8ff0f2f95b4d
youssef houmas <92693238+ysfzxz7@users.noreply.github.com>
2025-09-03 14:10:41 +0100

    add log in , profile , cart , reset password

---
+++ src/componants/popups/SmallCart.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+
+const SmallCart = () => {
+  const [qte, setQte] = useState(0);
+  const handleadd = () => {
+    setQte((prev) => prev + 1);
+  };
+  const handleLess = () => {
+    if (qte <= 0) {
+      return;
+    }
+    setQte((prev) => prev - 1);
+  };
+
+  return (
+    <div className="absolute bg-white top-0 right-0 m-10 flex px-5 py-2 items-center ">
+      <div className="absolute top-1 right-1">
+        <IoClose />
+      </div>
+
+      <div>
+        <img className="rounded" src="https://placehold.co/60x60" alt="" />
+      </div>
+      <div className="ml-5 flex flex-col mr-5">
+        <h2>Stylo</h2>
+        <h3>
+          Rest: <span>15</span>
+        </h3>
+        <div className="flex gap-3">
+          <button className="font-bold" onClick={handleadd}>
+            +
+          </button>
+          <span>{qte}</span>
+          <button className="font-bold cursor-pointer" onClick={handleLess}>
+            -
+          </button>
+        </div>
+        <button className="bg-green-600 text-white px-4 rounded text-xs py-[1px]">
+          add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SmallCart;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
+import ResetPassword from "./componants/login/ResetPassWord";
+import Profile from "./componants/user/Profile";
+import Cart from "./componants/Cart/Cart";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/ResetPassword",
+    element: <ResetPassword />,
+  },
+  {
+    path: "/Profile",
+    element: <Profile />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
   },
 ]);
 
